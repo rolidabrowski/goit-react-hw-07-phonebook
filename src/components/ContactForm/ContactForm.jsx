bb71cf15a4cbad45368479484c552fb8ff8e7140
Roland Dąbrowski <roli.dabrowski@gmail.com>
2023-07-17 20:21:23 +0200

    added validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,7 +1,33 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+import 'yup-phone';
 import { selectContacts } from 'redux/selectors';
 import { addContact } from 'redux/operations';
 import css from './ContactForm.module.css';
+
+const InitialValues = {
+  name: '',
+  phone: '',
+};
+
+const validationSchema = yup.object().shape({
+  name: yup
+    .string()
+    .matches(
+      /^[\s\p{L}]+$/u,
+      'Name may contain only letters and spaces. For example Adrian, Jacob Mercer and so on.'
+    )
+    .required(),
+  phone: yup
+    .string()
+    .phone(
+      'PL',
+      true,
+      'Phone number must be entered in the format 123-456-789 or 123 456 789 or 123456789'
+    )
+    .required(),
+});
 
 export const ContactForm = () => {
   const contacts = useSelector(selectContacts);
@@ -19,11 +45,8 @@ export const ContactForm = () => {
     return Boolean(duplicate);
   };
 
-  const saveContact = event => {
-    event.preventDefault();
-    const form = event.currentTarget;
-    const name = form.elements.name.value;
-    const phone = form.elements.number.value;
+  const saveContact = ({ name, phone }) => {
+    const form = document.querySelector('form');
     if (isDuplicate({ name, phone })) {
       return alert(
         contacts.find(contact => contact.name === name)
@@ -37,33 +60,29 @@ export const ContactForm = () => {
 
   return (
     <section className={css.form}>
-      <form onSubmit={saveContact}>
-        <label className={css.item}>
-          Name
-          <input
-            type="text"
-            name="name"
-            placeholder="Enter name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
-        <label className={css.item}>
-          Number
-          <input
-            type="tel"
-            name="number"
-            placeholder="Enter number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </label>
-        <button type="submit" className={css.button}>
-          Add contact
-        </button>
-      </form>
+      <Formik
+        initialValues={InitialValues}
+        onSubmit={values => {
+          saveContact({ ...values });
+        }}
+        validationSchema={validationSchema}
+      >
+        <Form>
+          <label className={css.item}>
+            Name
+            <Field type="text" name="name" placeholder="Enter name" />
+            <ErrorMessage name="name" component="span" />
+          </label>
+          <label className={css.item}>
+            Number
+            <Field type="tel" name="phone" placeholder="Enter number" />
+            <ErrorMessage name="phone" component="span" />
+          </label>
+          <button type="submit" className={css.button}>
+            Add contact
+          </button>
+        </Form>
+      </Formik>
     </section>
   );
 };
